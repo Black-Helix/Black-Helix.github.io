25ec542feed27aba48cc0943670b7197fc4a79a8
Calvin <cblack36@qub.ac.uk>
2024-12-13 03:51:45 +0000

    Confetti exp 12

--- bingo.js
+++ bingo.js
@@ -514,15 +514,15 @@ function updateParticles() {
 	var width = window.innerWidth;
 	var height = window.innerHeight;
 	var particle;
-	waveAngle += 0.01;
+	//waveAngle += 0.01;
 	for (var i = 0; i < particles.length; i++) {
 		particle = particles[i];
 		if (!streamingConfetti && particle.y < -15)
 			particle.y = height + 100;
 		else {
 			particle.tiltAngle += particle.tiltAngleIncrement;
-			particle.x += Math.sin(waveAngle) - 0.5;
-			particle.y += (Math.cos(waveAngle) + particle.diameter + confetti.speed) * 0.5;
+			//particle.x += Math.sin(waveAngle) - 0.5;
+			//particle.y += (Math.cos(waveAngle) + particle.diameter + confetti.speed) * 0.5;
 			particle.tilt = Math.sin(particle.tiltAngle) * 15;
 		}
 		if (particle.x > width + 20 || particle.x < -20 || particle.y > height) {
